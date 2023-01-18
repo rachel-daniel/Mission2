6dcca483ef6747373d57baf0bf5c49e949c2c30a
Rachel Daniel <racheldaniel328@gmail.com>
2023-01-18 14:23:59 -0700

    beginning code

--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -8,32 +8,38 @@ namespace Mission2
         {
             Random rand = new Random();
 
-            // int[] blah = {45, 20, 67, 10}; // fill the array when instantiating
-            int[] counter = new int[6]; // create a new array with null values
+            int[] counter = new int[11]; // create a new array with 11 null values
 
-            // loop to store the roll counts 1-6
-            for (int i = 0; i < 6; i++)
+            // initialize roll totals (between 2-12) at 0
+            for (int i = 0; i < 11; i++)
             {
                 counter[i] = 0; // fill the array with 0s for each value
             }
 
-            // loop to roll the dice 100 times (but the user will have the option to choose how many times to roll)
-            // keep track of how many times each die was rolled
-
-            Console.Write("Welcome to the dice throwing simulator!");
-            Console.Write("How many times would you like to roll the dice?");
+            // get number of rolls from user
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.WriteLine("How many times would you like to roll the dice?");
 
             string input = Console.ReadLine();
             int rolls = Convert.ToInt32(input);
 
-
+            // loop to keep track of the totals for x rolls
             for (int i = 0; i < rolls; i++)
             {
-                // Console.WriteLine(rand.Next(1, 7)); // prints the roll to the console
-                counter[rand.Next(6)]++;
+                // roll both dice and assign a random value from 1-6
+                int die1 = rand.Next(1, 7);
+                int die2 = rand.Next(1, 7);
+
+                // fill the counter array with the counts for each total
+                int total = die1 + die2;
+                counter[total]++;
             }
 
-            Console.Write("SIIMULATION RESULTS\nEach '*' represents 1% of total rolls\nThe total number of rolls=" + input);
+            // keep track of how many times each die was rolled
+            Console.WriteLine("SIMULATION RESULTS\nEach '*' represents 1% of total rolls\nThe total number of rolls=" + input);
+
+            // print histogram for percentage each number was rolled
+            // each * = 1% total rolls
 
             int number = 0;
 
